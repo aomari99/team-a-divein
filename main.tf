a1c568ecb79594659a5ba89da9a1681a0dcce681
thanhhadinh1912 <dinhthanhha1998@gmail.com>
2022-06-03 14:49:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,6 +64,45 @@ module "security_group" {
   tags = local.tags
 }
 
+
+
+variable "ecs_task_definition_family" {
+  description = "Specify a family for a task definition, which allows you to track multiple versions of the same task definition"
+  type        = string
+  default     = "wordpress-family"
+}
+
+variable "ecs_task_definition_cpu" {
+  description = "Number of CPU units reserved for the container in powers of 2"
+  type        = string
+  default     = "1024"
+}
+
+variable "ecs_task_definition_memory" {
+  description = "Specify a family for a task definition, which allows you to track multiple versions of the same task definition"
+  type        = string
+  default     = "2048"
+}
+
+variable "db_master_username" {
+  description = "Master username of the db"
+}
+variable "db_master_password" {
+  description = "Master password of the db"
+}
+
+variable "ecs_service_name" {
+  description = "Name for the ECS Service"
+  type        = string
+  default     = "wordpress"
+}
+
+variable "ecs_service_desired_count" {
+  description = "The number of instances of fargate tasks to keep running"
+  type        = number
+  default     = 1
+}
+
 ################################################################################
 # Master DB
 ################################################################################
@@ -100,6 +139,209 @@ module "master" {
   deletion_protection     = false
 
   tags = local.tags
+}
+
+resource "aws_ecs_cluster" "wordpress" {
+  name = var.ecs_cluster_name
+}
+resource "aws_security_group" "wordpress" {
+  name        = "$wordpress-sg"
+  description = "Fargate wordpress security group"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress{
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.tags
+}
+
+
+//Defining all parameters related to wordpress container
+resource "aws_ecs_task_definition" "wordpress" {
+  family                   = var.ecs_task_definition_family
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.ecs_task_definition_cpu
+  memory                   = var.ecs_task_definition_memory
+  container_definitions    = <<CONTAINER_DEFINITION
+[
+  {
+    "secrets": [
+      {
+        "name": "WORDPRESS_DB_USER", 
+        "valueFROM": "test"
+      },
+      {
+        "name": "WORDPRESS_DB_PASSWORD", 
+        "valueFROM": "test"
+      }
+    ],
+    "environment": [
+      {
+        "name": "WORDPRESS_DB_HOST",
+        "value": "${module.master.this.endpoint}"
+      },
+      {
+        "name": "WORDPRESS_DB_NAME",
+        "value": "wordpress"
+      }
+    ],
+    "essential": true,
+    "image": "wordpress:latest",        
+    "name": "wordpress",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "protocol": "tcp"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver":"awslogs",
+      "options": {
+        "awslogs-group": "${aws_cloudwatch_log_group.wordpress.name}",
+        "awslogs-region": "${data.aws_region.current.name}",
+        "awslogs-stream-prefix": "app"
+      }
+    }
+  }
+]
+
+CONTAINER_DEFINITION
+
+}
+
+resource "aws_cloudwatch_log_group" "wordpress" {
+  name              = "/${var.prefix}/${var.environment}/fg-task"
+  tags              = var.tags
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_group" "wordpress" {
+  name              = "/ecs/wordpress"
+  description       = " Log Group where ECS logs should be written"
+  tags              = var.tags
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_ecs_service" "wordpress" {
+  name             = var.ecs_service_name
+  cluster          = aws_ecs_cluster.wordpress.id
+  task_definition  = aws_ecs_task_definition.wordpress.arn
+  desired_count    = var.ecs_service_desired_count
+  launch_type      = "FARGATE"
+  platform_version = "1.4.0" 
+  network_configuration {
+    security_groups = [aws_security_group.db.id]
+    subnets         = module.vpc.public_subnets
+  }
+
+}
+
+ data "aws_iam_policy_document" "kms" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "ArnLike"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+    }
+  }
+}
+ 
+
+
+ 
+
+
+
+data "aws_iam_policy_document" "ecs_task_trust" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_task_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  
+  statement {
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
+
+
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "wordpressTaskRole"
+  description = "IAM Role for task to use to access AWS services (dynamo, s3, etc.)"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_trust.json
+}
+
+resource "aws_iam_policy" "ecs_task_policy" {
+  name   = "wordpressTaskPolicy"
+  policy = data.aws_iam_policy_document.ecs_task_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_role_attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_task_policy.arn
 }
 
 ################################################################################
