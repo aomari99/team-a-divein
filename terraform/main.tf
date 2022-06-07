b3548cb1381b9ee88ee36854e684e5e6953d51e2
Adam Omari <52887341+aomari99@users.noreply.github.com>
2022-06-07 13:09:28 +0200

    uncomment backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,10 @@
-/*terraform {
+terraform {
   backend "s3" {
     bucket = "wordpress-teama"
-    key    = "test"
     region = "eu-west-1"
   }
-}*/
+}
 provider "aws" {
   region = local.region
 }
 
-locals {
-  name   = "wordpress-db"
-  region = "eu-west-1"
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-
-  engine                = "mysql"
-  engine_version        = "8.0.27"
-  family                = "mysql8.0" # DB parameter group
-  major_engine_version  = "8.0"      # DB option group
-  instance_class        = "db.t4g.micro" 
-  allocated_storage     = 20
-  max_allocated_storage = 100
-  port                  = 3306
-}
